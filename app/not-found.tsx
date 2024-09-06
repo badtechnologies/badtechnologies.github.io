ea99e47379f7dd4e2a47b638634e9be5b8999951
Logan Dhillon <logandhi64@gmail.com>
2024-09-06 10:36:16 -0400

    Make 404 page buttons work

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,21 @@
+"use client";
+
 import { Button } from "@nextui-org/button";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="max-w-96 mx-auto flex flex-col text-center items-center gap-8 my-12">
       <h1 className="text-3xl font-semibold">
-        The page you're looking for can't be found.{" "}
+        The page you're looking for can't be found.
       </h1>
       <div className="flex gap-2">
-        <Button color="primary" radius="full" className="w-28">
+        <Button
+          color="primary"
+          radius="full"
+          className="w-28"
+          onPress={() => window.history.back()}
+        >
           Go back
         </Button>
         <Button
@@ -15,6 +23,8 @@ export default function NotFound() {
           radius="full"
           className="w-28"
           variant="bordered"
+          as={Link}
+          href="/"
         >
           Go home
         </Button>
